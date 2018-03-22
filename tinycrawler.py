204c351a6bc7c5f27fd86d8638f39f42b9e90576
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-03-22 22:17:58 +0100

    Fixed various bugs introduced with refactoring

--- tinycrawler.py
+++ tinycrawler.py
@@ -28,12 +28,12 @@ class TinyCrawler:
         self.domain = self._get_domain(url_seed)
         self._init_paths()
         self._init_log()
-        self._init_urls()
+        self._init_urls(url_seed)
 
     def _init_paths(self):
         self.directory = "%s/%s"%(self.directory, self.domain)
         self.urlsPath = "%s-urls.json"%(self.directory)
-        self.logPath = "%s/%s.log"%(self.directory)
+        self.logPath = "%s.log"%(self.directory)
 
         if not os.path.exists(self.directory):
             os.makedirs(self.directory)
@@ -45,7 +45,7 @@ class TinyCrawler:
                 pass
         logging.basicConfig(filename=self.logPath,level=logging.ERROR)
 
-    def _init_urls(self):
+    def _init_urls(self, url_seed):
         if os.path.isfile(self.urlsPath):
             with open(self.urlsPath) as json_data:
                 data = json.load(json_data)
@@ -108,11 +108,11 @@ class TinyCrawler:
 
         soup = BeautifulSoup(request.text, 'lxml')
 
-        outgoing_urls = self._extract_urls(url, soup)
+        new_urls = self._extract_urls(url, soup)
 
-        self._cache_url(url, path, outgoing_urls, soup)
+        self._cache_url(url, path, new_urls, soup)
 
-        return outgoing_urls
+        return new_urls
 
     def _load_cached_webpage(self, path):
         self.wait = False
@@ -129,7 +129,7 @@ class TinyCrawler:
     def _url_filter(self, url):
         return self.domain == self._get_domain(url) and self.custom_url_filter(url) and url not in self.urls
 
-    def _sleep(self, start, end):
+    def _sleep(self, start):
         if self.wait:
             sleep_time = random.randint(self.minTime,self.maxTime)/1000-time.time()+start
             if sleep_time > 0:
@@ -146,7 +146,8 @@ class TinyCrawler:
     def _iterate(self, url):
         try:
             for new_url in self._parse_url(url):
-                if self._url_filter(url):
+                #print(new_url)
+                if self._url_filter(new_url):
                     self.urls.append(new_url)
             self._update_stored_urls()
         except Exception as e:
@@ -156,44 +157,43 @@ class TinyCrawler:
     def _estimated_time(self):
         d = datetime(1,1,1) + timedelta(seconds=self.estimated_step_time*(len(self.urls)-self.url_number))
         response = ""
-        if d.day>0:
-            response += "%s d"%(d.day-1)
+        if d.day-1>0:
+            response += "%sd"%(d.day-1)
 
         if d.hour>0:
             if response != "":
                 response+=", "
-            response += "%s h"%(d.hour)
+            response += "%sh"%(d.hour)
 
         if d.minute>0:
             if response != "":
                 response+=", "
-            response += "%s m"%(d.minute)
+            response += "%sm"%(d.minute)
 
         if d.second>0:
             if response != "":
                 response+=", "
-            response += "%s s"%(d.second)
+            response += "%ss"%(d.second)
         return response
 
     def _update_bar(self):
-        print ("%s: %s out of %s. Estimated remaining time: %s"%(self.domain, self.url_number, len(self.urls), self._estimated_time()), end="\r")
+        print ("%s: %s/%s. ETA: %s"%(self.domain, self.url_number, len(self.urls), self._estimated_time()), end="\r")
         sys.stdout.flush()
 
     def set_url_filter(self, function):
         self.custom_url_filter = function
 
     def run(self, iterations_number = -1):
-    	if iterations_number != -1:
-	        while self.url_number < iterations_number:
-                start = time.time()
-	            self._iterate(self.urls[self.url_number])
-	            self._sleep(start)
-                self._update_bar()
-                self.url_number+=1
+        while (self.url_number < iterations_number or iterations_number == -1) and  self.url_number < len(self.urls):
+            start = time.time()
+            self._iterate(self.urls[self.url_number])
+            self._sleep(start)
+            self._update_bar()
+            self.url_number+=1
 
 def myCustomFilter(url):
     for unwanted_word in ["#", "forum"]:
-        if useless_word in url:
+        if unwanted_word in url:
             return False
     return True
 
