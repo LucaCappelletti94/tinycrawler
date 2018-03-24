e5cd1bfb1fb1251a17a8828b59e80ee995a4cfdf
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-03-24 17:34:09 +0100

    Ended refactoring

--- tinycrawler.py
+++ tinycrawler.py
@@ -18,6 +18,8 @@ from tinycrawler.bar import bar
 
 class TinyCrawler:
 
+    _processes_number = 8
+
     def __init__(self, seed, directory = "../downloaded_websites"):
         self._domain = self._get_domain(seed)
         self._directory = "%s/%s"%(self._directory, self._domain)
@@ -31,16 +33,16 @@ class TinyCrawler:
         )
         self._proxies = Proxies()
 
+    def _get_domain(self, url):
+        parsed_uri = urlparse(url)
+        return '{uri.netloc}'.format(uri=parsed_uri)
+
     def _get_clean_text(self, soup):
         for useless_tag in ["form", "script", "head", "style", "input"]:
             [s.extract() for s in soup(useless_tag)]
         clean_text = soup.get_text()
         clean_text = " ".join(clean_text.split())
         return clean_text
-
-    def _get_domain(self, url):
-        parsed_uri = urlparse(url)
-        return '{uri.netloc}'.format(uri=parsed_uri)
 
     def _urls_from_soup(self, base, soup):
         urls = []
@@ -75,82 +77,63 @@ class TinyCrawler:
     def _request_is_binary(self, request):
         return 'text/html' not in request.headers['content-type']
 
-    def _download_new_webpage(self, url, proxy, path):
-        self.wait = True
+    def _download(self, url, path):
+
+
+        # If there are no free proxies, we sleep
+        while self._proxies.empty():
+            time.sleep(0.1)
+        # When there is one, we aquire lock
+        proxy = self._proxies.get()
 
         try:
             request = requests.get(url, proxies = self._proxy_to_urls(proxy))
+            if self._request_is_binary(request):
+                return []
+
+            soup = BeautifulSoup(request.text, 'lxml')
+
+            new_urls = self._urls_from_soup(url, soup)
+
+            self.urls.add_list(new_urls)
+
+            self._cache_webpage(url, path, new_urls, soup)
         except Exception as e:
-            return False
+            pass
 
-        if self._request_is_binary(request):
-            return []
+        # When we are done, we free the proxy
+        self._proxies.add(proxy)
 
-        soup = BeautifulSoup(request.text, 'lxml')
-
-        new_urls = self._urls_from_soup(url, soup)
-
-        self.urls.add_list(new_urls)
-
-        self._cache_webpage(url, path, new_urls, soup)
-
-        return new_urls
 
     def _load_cached_webpage(self, path):
         self.wait = False
         return self._load_cached_urls(path)
 
-    def _parse_url(self, url_proxy_tuple):
-        url = url_proxy_tuple[0]
-        proxy = url_proxy_tuple[1]
-        path = self._get_url_path(url)
 
-        if self._is_path_cached(path):
-            return self._load_cached_webpage(path)
+    def _job(self, lock):
+        while not self._urls.empty():
+            url = self._urls.get()
 
-        return self._download_new_webpage(url, proxy, path)
+            path = self._get_url_path(url)
 
-    def _sleep(self, start):
-        if self.wait:
-            self.sleep_time = random.randint(self.min_time,self.max_time)/1000-time.time()+start
-            if self.sleep_time > 0:
-                time.sleep(self.sleep_time)
+            if self._is_path_cached(path):
+                self._load_cached_webpage(path)
 
-    def _update_stored_urls(self):
-        with open(self._urlsPath, 'w') as outfile:
-            json.dump({
-                "url_number":self.url_number,
-                "urls":self.urls
-            }, outfile)
-
-    def _get_proxy_url_map(self):
-        proxy_urls_map = []
-        for i, url in enumerate(self.urls[self.url_number: min(self.url_number + len(self.proxy_list), len(self.urls))]):
-            proxy_urls_map.append((url, self.proxy_list[i]))
-
-        return proxy_urls_map
-
-    def _iterate(self, proxy_urls_map):
-        with multiprocessing.Pool(self.thread_number) as p:
-            new_urls_chunks = list(p.imap(self._parse_url, proxy_urls_map))
-        new_urls = [item for items in new_urls_chunks for item in items]
-        for new_url in new_urls:
-            if self._url_filter(new_url):
-                self.to_be_parsed_urls.append(new_url)
-        self._update_stored_urls()
-
-        self.url_number+=len(proxy_urls_map)
+            self._download(url,path)
 
     def set_url_filter(self, function):
         self.urls.set_url_filter(function)
 
-    def run(self, iterations_number = -1):
-        while (self.url_number < iterations_number or iterations_number == -1) and  self.url_number < len(self.urls):
-            start = time.time()
-            self._iterate(self._get_proxy_url_map())
-            self._update_estimated_time(start)
-            self._sleep(start)
-            self._update_bar()
+    def run(self):
+        processes = []
+        lock = Lock()
+        for i in range(self._processes_number):
+            p = Process(target=self._job, args=(lock))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
 
 def myCustomFilter(url):
     for unwanted_word in ["#", "forum"]:
