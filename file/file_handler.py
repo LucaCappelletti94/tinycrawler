dc334b0b9a0f072bd76b714de575c0c1730e6d2e
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-04-11 10:40:00 +0200

    Splitted file parser into two processes

--- file/file_handler.py
+++ file/file_handler.py
@@ -1,5 +1,8 @@
 from multiprocessing import Queue
 
+from urllib.parse import urljoin, urlparse
+import validators
+
 from .file_parser import file_parser
 from .file_writer import file_writer
 
@@ -10,11 +13,16 @@ class file_handler:
         parsed = Queue()
         graph = Queue()
 
-        self._parser = file_parser(
-            files = files,
-            parsed = parsed,
-            urls = urls,
-            graph = graph,
+        self._file_parser = file_parser(
+            input_queue = files[0],
+            output_queue = parsed,
+            statistics = statistics,
+            logger = logger,
+            timeout = timeout
+        )
+        self._url_parser = file_parser(
+            input_queue = files[1],
+            output_queue = graph,
             statistics = statistics,
             logger = logger,
             timeout = timeout
@@ -24,16 +32,32 @@ class file_handler:
 
     def run(self):
         """Starts the parser"""
-        self._parser.run()
+        self._url_parser.set_custom_parser(self._extract_valid_urls)
+        self._file_parser.run()
+        self._url_parser.run()
         self._webpages_writer.run()
         self._graph_writer.run()
 
     def join(self):
-        self._parser.join()
+        self._url_parser.join()
+        self._file_parser.join()
         self._webpages_writer.join()
         self._graph_writer.join()
 
+    def _extract_valid_urls(self, request_url, soup):
+        urls = []
+        for link in soup.find_all('a', href=True):
+            url = urljoin(request_url, link["href"])
+            if validators.url(url):
+                urls.append(url)
+                if self._custom_url_validator(url) and not self._urls.contains(url):
+                    self._urls.put(url)
         self._statistics.add_done()
         self._statistics.add_total(len(urls))
+        return urls
+
     def set_url_validator(self, url_validator):
-        self._parser.set_url_validator(url_validator)
+        self._custom_url_validator = url_validator
+
+    def set_file_parser(self, parser):
+        self._file_parser.set_custom_parser(parser)
--- file/file_parser.py
+++ file/file_parser.py
@@ -1,66 +1,27 @@
-from urllib.parse import urljoin, urlparse
-import validators
 import hashlib
-from bs4 import BeautifulSoup
 from ..process.process_handler import process_handler
 
 class file_parser(process_handler):
 
-    def __init__(self, files, parsed, urls, graph, statistics, logger, timeout):
+    def __init__(self, input_queue, output_queue, statistics, logger, timeout):
         super().__init__(statistics, logger)
-        self._files = files # Queue of files to be parsed
-        self._parsed = parsed # Queue of parsed files
-        self._urls = urls # UrlQueue of parsed urls
-        self._graph = graph # Queue of urls per file
+        self._input_queue = input_queue # Queue of files to be parsed
+        self._output_queue = output_queue # Queue of parsed files
         self._timeout = timeout
-        self._custom_file_parser = lambda soup: soup
-        self._custom_url_validator = lambda url: True
-        self._total_urls = 0
-        self._done_urls = 0
-
-    def _valid_url(self, url):
-        return validators.url(url)
-
-    def _is_url_downloadable(self, url):
-        return self._custom_url_validator(url) and not self._urls.contains(url)
-
-    def _extract_valid_urls(self, request_url, soup):
-        urls = []
-        for link in soup.find_all('a', href=True):
-            url = urljoin(request_url, link["href"])
-            if self._valid_url(url):
-                urls.append(url)
-                if self._is_url_downloadable(url):
-                    self._urls.put(url)
-                    self._total_urls += 1
-        return urls
+        self._custom_parser = lambda request_url, soup: soup
 
     def _parse(self):
         """Parse the downloaded files, cleans them and extracts urls"""
-        request_url, file = self._files.get(timeout=self._timeout)
-        soup = BeautifulSoup(file, 'lxml')
-        filename = hashlib.md5(urlparse(request_url).path.encode('utf-8')).hexdigest()
-        self._parsed.put((filename,{
+        request_url, file_soup = self._files.get(timeout=self._timeout)
+        filename = hashlib.md5(request_url.encode('utf-8')).hexdigest()
+        self._output_queue.put((filename,{
             "url": request_url,
-            "content": str(self._custom_file_parser(soup))
-        }))
-        self._graph.put((filename,{
-            "incoming": request_url,
-            "outgoing": self._extract_valid_urls(request_url, soup)
+            "content": self._custom_parser(request_url, file_soup)
         }))
 
-        self._done_urls += 1
-
-        self._statistics.set_done(self._done_urls)
-        self._statistics.set_total(self._total_urls)
-
-    def set_url_validator(self, custom_url_validator):
+    def set_custom_parser(self, custom_parser):
         """Sets the user defined url parser"""
-        self._custom_url_validator = custom_url_validator
-
-    def set_custom_file_parser(self, custom_file_parser):
-        """Sets the user defined file parser"""
-        self._custom_file_parser = custom_file_parser
+        self._custom_parser = custom_parser
 
     def run(self):
         """Starts the parser"""
