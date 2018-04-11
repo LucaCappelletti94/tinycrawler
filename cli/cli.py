83f9d3b2a8a5a0fd5b34c884f3fe82b80c3bf9dd
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-04-11 09:08:55 +0200

    Added padding to cli

--- cli/cli.py
+++ cli/cli.py
@@ -11,6 +11,7 @@ class cli:
         self._i=0
         self._outputs_max_lenghts = {}
         self._speeds = []
+        self._outputs = {}
         self._old_done = 0
 
     def _cli(self):
@@ -21,39 +22,53 @@ class cli:
         while True:
             time.sleep(1)
 
-            for k, v in self._outputs_max_lenghts.items():
-                self._print(" "*v, k)
+            self._clear()
 
-            self._stdscr.refresh()
+            done = self._statistics.get_done()
+            total = self._statistics.get_total()
+            failed = self._statistics.get_failed()
 
-            self._reset_cursor()
-
-            self._print("Downloaded pages: %s/%s"%(
-                self._statistics.get_done(),
-                self._statistics.get_total()
-            ), self._i)
-
-            self._print("Failed pages: %s/%s"%(
-                self._statistics.get_failed(),
-                self._statistics.get_done()
-            ), self._i)
-
-            self._speeds.append(self._statistics.get_done()-self._old_done)
-            self._speeds = self._speeds[-1000:]
-            self._old_done = self._statistics.get_done()
-
-            self._print("Average speed: %s it/s"%(
-                round(sum(self._speeds) / len(self._speeds),2)
-            ), self._i)
+            self._print_fraction("Downloaded pages", done, total)
+            self._print_fraction("Failed pages", failed, done)
+            self._print_speed()
 
             self._print_frame(0)
             self._print_frame(self._i-1)
+
+            self._print_all()
 
             self._stdscr.refresh()
 
         curses.echo()
         curses.nocbreak()
         curses.endwin()
+
+    def _print_fraction(self, label, v1, v2):
+        if v2 == 0:
+            perc = "NaN"
+        else:
+            perc = str(round(v1/v2*100, 2))+"%"
+        self._print("%s: §%s/%s, %s"%(
+            label,
+            v1,
+            v2,
+            perc
+        ), self._i)
+
+    def _clear(self):
+        for k, v in self._outputs_max_lenghts.items():
+            self._stdscr.addstr(k, 0, " "*v)
+        self._stdscr.refresh()
+        self._reset_cursor()
+
+    def _print_speed(self):
+        self._speeds.append(self._statistics.get_done()-self._old_done)
+        self._speeds = self._speeds[-1000:]
+        self._old_done = self._statistics.get_done()
+
+        self._print("Average speed: §%s it/s"%(
+            round(sum(self._speeds) / len(self._speeds),2)
+        ), self._i)
 
     def _reset_cursor(self):
         self._i = 1
@@ -70,8 +85,19 @@ class cli:
             pos: max(old_max, len(value))
         })
 
-        self._stdscr.addstr(pos, 0, value)
+        self._outputs.update({
+            pos: value
+        })
         self._i+=1
+
+    def _print_all(self):
+        max_len = max(self._outputs_max_lenghts.values())
+        for k, v in self._outputs.items():
+            if "§" in v:
+                a, b = v.split("§")
+                padding = " "*(max_len-len(v))
+                v = a+padding+b
+            self._stdscr.addstr(k, 0, v)
 
     def run(self):
         self._cli_process = Process(target=self._cli)
