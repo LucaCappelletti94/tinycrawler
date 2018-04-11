8c5fd2e5017e2caaed78d9a5fa132133719f6831
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-04-11 14:44:32 +0200

    Added total proxies and elapsed time statistics

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -1,4 +1,6 @@
 from multiprocessing import Lock
+from datetime import datetime, timedelta
+import time
 
 class statistics:
     def __init__(self):
@@ -7,6 +9,9 @@ class statistics:
         self._done = 0
         self._total = 0
         self._failed = 0
+        self._total_proxies = 0
+        self._active_proxies = 0
+        self._start_time = time.time()
 
     def set_process_running(self, name, status):
         self._lock.acquire()
@@ -26,6 +31,9 @@ class statistics:
         self._failed += 1
         self._lock.release()
 
+    def set_total_proxies(self, total_proxies):
+        self._total_proxies = total_proxies
+
     def get_done(self):
         return self._done
 
@@ -37,3 +45,33 @@ class statistics:
 
     def get_running_processes(self):
         return self._running_processes
+
+    def get_total_proxies(self):
+        return self._total_proxies
+
+    def _format_value(self,response,value,pattern):
+        if value > 0:
+            if response != "":
+                response+=", "
+            response += pattern%value
+        return response
+
+    def _seconds_to_string(self, delta):
+
+        if delta <= 0:
+            return "now"
+
+        d = datetime(1,1,1) + timedelta(seconds=delta)
+
+        eta = ""
+        if d.day-1>0:
+            eta += "%sd"%(d.day-1)
+
+        eta = self._format_value(eta,d.hour,  "%sh")
+        eta = self._format_value(eta,d.minute, "%sm")
+        eta = self._format_value(eta,d.second,"%ss")
+
+        return eta
+
+    def get_elapsed_time(self):
+        return self._seconds_to_string(time.time()-self._start_time)
