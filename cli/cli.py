f884d0f1bdb03a99b2d985c93f9cc53c71dd6594
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-04-12 15:58:03 +0200

    Cli shows more infos

--- cli/cli.py
+++ cli/cli.py
@@ -10,35 +10,52 @@ class cli:
         self._statistics = statistics
         self._i=0
         self._outputs_max_lenghts = {}
-        self._speeds = []
-        self._growing_speeds = []
         self._outputs = {}
-        self._old_done = 0
-        self._old_total = 0
+        self._estimate_update_timeout = 10
+        self._last_estimate_update = 0
 
     def _cli(self):
-        time.sleep(1)
         self._statistics.set_start_time()
         self._stdscr = curses.initscr()
         curses.noecho()
         curses.cbreak()
         while True:
-            time.sleep(1)
-
+            time.sleep(0.1)
             self._clear()
 
             done = self._statistics.get_done()
             total = self._statistics.get_total()
             failed = self._statistics.get_failed()
+            binary = self._statistics.get_binary_requests()
+            total_downloaders = self._statistics.get_total_downloaders()
 
             self._print_fraction("Downloaded pages", done, total)
-            self._print_fraction("Failed pages", failed, done)
+            if failed != 0:
+                self._print_fraction("Failed pages", failed, done)
+            if binary != 0:
+                self._print_fraction("Binary requests", binary, done)
+
+            for code, number in self._statistics.get_error_codes().items():
+                self._print_fraction("Error code %s"%code, number, done)
             self._print_fraction("Free proxies", self._statistics.get_free_proxies(), self._statistics.get_total_proxies())
-            self._print_fraction("Downloaders waiting proxy", self._statistics.get_processes_waiting_proxies(), self._statistics.get_total_downloaders())
-            self._print_fraction("Downloaders waiting urls", self._statistics.get_processes_waiting_urls(), self._statistics.get_total_downloaders())
-            self._print_speed()
-            self._print_growing_speed()
-            self._print("Elapsed time: §%s"% self._statistics.get_elapsed_time())
+            self._print_fraction("Downloaders waiting proxy", self._statistics.get_processes_waiting_proxies(), total_downloaders)
+            self._print_fraction("Downloaders waiting urls", self._statistics.get_processes_waiting_urls(), total_downloaders)
+
+            elaboration_speed = self._statistics.get_elaboration_speed()
+            growth_speed = self._statistics.get_growth_speed()
+
+            if elaboration_speed != 0:
+                self._print_label("Elaboration speed", str(round(elaboration_speed, 2))+" url/s")
+
+            if growth_speed != 0:
+                self._print_label("Pool growth speed", str(round(growth_speed, 2))+" url/s")
+
+            if time.time() - self._last_estimate_update > self._estimate_update_timeout:
+                self._elapsed_time = self._statistics.get_remaining_time()
+                self._last_estimate_update = time.time()
+
+            self._print_label("Elapsed time", self._statistics.get_elapsed_time())
+            self._print_label("Estimated remaining time", self._elapsed_time)
 
             self._print_frame(0)
             self._print_frame(self._i-1)
@@ -52,49 +69,20 @@ class cli:
         curses.endwin()
 
     def _print_fraction(self, label, v1, v2):
-        if v2 == 0:
-            perc = "NaN"
-        else:
+        if v2 != 0:
             perc = str(round(v1/v2*100, 1))+"%"
 
-        self._print("%s: §%s/%s %s"%(
-            label,
-            v1,
-            v2,
-            perc
-        ))
-
-    def _clear(self):
-        for k, v in self._outputs_max_lenghts.items():
-            self._stdscr.addstr(k, 0, " "*v)
-        self._stdscr.refresh()
-        self._reset_cursor()
-
-    def _print_speed(self):
-        done = self._statistics.get_done()
-        self._speeds.append(done-self._old_done)
-        self._speeds = self._speeds[-1000:]
-        self._old_done = done
-
-        self._print("Average speed: §%s it/s"%(
-            round(sum(self._speeds) / len(self._speeds),2)
-        ))
-
-    def _print_growing_speed(self):
-        total = self._statistics.get_total()
-        self._growing_speeds.append(total-self._old_total)
-        self._growing_speeds = self._growing_speeds[-1000:]
-        self._old_total = total
-
-        self._print("Growing speed: §%s url/s"%(
-            round(sum(self._growing_speeds) / len(self._growing_speeds),2)
-        ))
-
-    def _reset_cursor(self):
-        self._i = 1
+            self._print_label(label, "%s/%s %s"%(
+                v1,
+                v2,
+                perc
+            ))
 
     def _print_frame(self, pos):
         self._print("-"*max(self._outputs_max_lenghts.values()), pos)
+
+    def _print_label(self, label, value, pos=None):
+        self._print("%s: §%s"%(label, value), pos)
 
     def _print(self, value, pos=None):
         if pos == None:
@@ -120,6 +108,18 @@ class cli:
                 padding = " "*(max_len-len(v))
                 v = a+padding+b
             self._stdscr.addstr(k, 0, v)
+
+    def _clear(self):
+        if len(self._outputs_max_lenghts.values()) > 0:
+            max_len = max(self._outputs_max_lenghts.values())
+        max_len = 0
+        for i in range(self._i):
+            self._stdscr.addstr(i, 0, " "*max_len)
+        self._stdscr.refresh()
+        self._reset_cursor()
+
+    def _reset_cursor(self):
+        self._i = 1
 
     def run(self):
         self._cli_process = Process(target=self._cli, name="cli")
