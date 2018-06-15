6ef43481ba88d44d33765668ae8ee6ea029f7394
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-06-15 17:32:14 +0200

    Removed commented code

--- tinycrawler/log/log.py
+++ tinycrawler/log/log.py
@@ -14,20 +14,10 @@ class Log:
         """Log message with lock semaphore in info level."""
         self._lock.acquire()
         logging.info(message)
-        # TODO remove this ugly commented code
-        # with open(self._path, "r") as f:
-        #     data = f.read()
-        # with open(self._path, "w") as f:
-        #     f.write(data + "\n" + message)
         self._lock.release()
 
     def error(self, message):
         """Log message with lock semaphore in error level."""
         self._lock.acquire()
         logging.error(message)
-        # TODO remove this ugly commented code
-        # with open(self._path, "r") as f:
-        #     data = f.read()
-        # with open(self._path, "w") as f:
-        #     f.write(data + "\n" + message)
         self._lock.release()
