40c305ee0d747de502e31fd391a356bee1386398
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-06-18 15:30:00 +0200

    Refactored speed to sliding window

--- tinycrawler/statistics/speed.py
+++ tinycrawler/statistics/speed.py
@@ -3,14 +3,18 @@ from time import time
 
 class Speed:
     SECONDS = "s"
+    INTERVAL = 1 * 60
 
     def __init__(self, unit):
         self._unit = unit
-        self._total = 0
+        self._total = []
         self._start = time()
 
     def update(self, value):
-        self._total += value
+        self._total.append({
+            "value": value,
+            "timestamp": time()
+        })
 
     def _format_speed(self):
         speed = self.get_speed()
@@ -28,8 +32,17 @@ class Speed:
         return "%s%s %s" % (round(speed, 2), u, unit)
 
     def get_speed(self):
-        delta = time() - self._start
-        return self._total / delta
+        now = time()
+        current = now - self.INTERVAL
+        delta = min(self.INTERVAL, now - self._start)
+        total = 0
+        for i, info in reversed(list(enumerate(self._total))):
+            if info["timestamp"] < current:
+                break
+            total += info["value"]
+        self._total = self._total[i:]
+
+        return total / delta
 
     def get_formatted_speed(self):
         return self._format_speed()
