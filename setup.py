8fd69f29ab0525672caf5919a8d7568970374728
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-10-04 12:02:41 +0200

    Added pybloom required package in setup

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['validators', 'requests']
+    install_requires=['validators', 'requests', 'pybloom_live']
 )
 
 
