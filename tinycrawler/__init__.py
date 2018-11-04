a20dbacde68c15873fe230f7cf95882e82ecaaaa
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-11-04 10:59:15 +0100

    Exposed UrlJob and Statistics from package.

--- tinycrawler/__init__.py
+++ tinycrawler/__init__.py
@@ -1,5 +1,7 @@
 """Import tinycrawler class."""
 from .tinycrawler import TinyCrawler
 from .log import Log
+from .job import UrlJob
+from .statistics import Statistics
 
-__all__ = ['TinyCrawler', 'Log']
+__all__ = ['TinyCrawler', 'Log', 'UrlJob', 'Statistics']
