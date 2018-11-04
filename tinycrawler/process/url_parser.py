a635bc351eaf7366bfabeec86f51031ff5590e15
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-11-04 10:01:15 +0100

    Extended typization and now url methods receive also statistics.

--- tinycrawler/process/url_parser.py
+++ tinycrawler/process/url_parser.py
@@ -12,34 +12,40 @@ from validators import url as valid
 
 from .parser import Parser
 from ..log import Log
+from ..statistics import Statistics
+from ..job import UrlJob, FileJob
 
 
 class UrlParser(Parser):
 
-    def __init__(self, path: str, jobs, urls):
+    def __init__(self, path: str, jobs: FileJob, urls: UrlJob):
         super().__init__(path + "/graph", "urls parser", jobs)
         self._regex = re.compile(r"href=\"([^\"#]+)\"")
         self._val = self._tautology
         self._urls = urls
         self._url_extractor = self._default_url_extractor
 
-    def _tautology(self, url: str, logger: Log):
+    def _tautology(self, url: str, logger: Log, statistics: Statistics):
         return True
 
-    def _default_url_extractor(self, response: Response, urls, logger: Log):
+    def _default_url_extractor(self, response: Response, urls: UrlJob, logger: Log, statistics: Statistics):
         url = response.url
         for partial_link in BeautifulSoup(response.text, "lxml").findAll("a",  href=True):
             link = urljoin(url, partial_link["href"])
             if valid(link) and self._val(link, self._logger):
                 urls.put(link)
 
-    def _parser(self, response: Response, logger: Log):
-        self._url_extractor(response, self._urls, logger)
+    def _parser(self, response: Response, logger: Log, statistics: Statistics):
+        self._url_extractor(response, self._urls, logger, statistics)
 
-    def set_validator(self, url_validator: Callable[[str, Log], bool]):
-        """Set custom url validator."""
+    def set_validator(self, url_validator: Callable[[str, Log, Statistics], bool]):
+        """Set custom url validator.
+            url_validator: Callable[[str, Log, Statistics], bool], the function used to validate urls.
+        """
         self._val = url_validator
 
-    def set_url_extractor(self, url_extractor):
-        """Set custom url extractor."""
+    def set_url_extractor(self, url_extractor: Callable[[Response, UrlJob, Log, Statistics], None]):
+        """Set custom url extractor.
+            url_extractor: Callable[[Response, UrlJob, Log, Statistics], None], the function used to extract urls.
+        """
         self._url_extractor = url_extractor
