34870059685b5fc5b070241640defc0c8f83c099
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-11-04 09:31:36 +0100

    Started to typize classes.

--- tinycrawler/process/url_parser.py
+++ tinycrawler/process/url_parser.py
@@ -5,6 +5,7 @@ import re
 from multiprocessing import cpu_count
 from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
+from requests import Response
 
 from validators import url as valid
 
@@ -14,7 +15,7 @@ from ..log import Log
 
 class UrlParser(Parser):
 
-    def __init__(self, path, jobs, urls):
+    def __init__(self, path: str, jobs, urls):
         super().__init__(path + "/graph", "urls parser", jobs)
         self._regex = re.compile(r"href=\"([^\"#]+)\"")
         self._val = self._tautology
@@ -24,14 +25,14 @@ class UrlParser(Parser):
     def _tautology(self, url: str, logger: Log):
         return True
 
-    def _default_url_extractor(self, response, urls, logger):
+    def _default_url_extractor(self, response: Response, urls, logger: Log):
         url = response.url
         for partial_link in BeautifulSoup(response.text, "lxml").findAll("a",  href=True):
             link = urljoin(url, partial_link["href"])
             if valid(link) and self._val(link, self._logger):
                 urls.put(link)
 
-    def _parser(self, response, logger):
+    def _parser(self, response: Response, logger: Log):
         self._url_extractor(response, self._urls, logger)
 
     def set_validator(self, url_validator):
