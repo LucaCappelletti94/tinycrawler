029c9394e7910e89649823314ca1c03c3d631044
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-11-05 09:24:28 +0100

    Removed hash function from urljob

--- tinycrawler/job/urljob.py
+++ tinycrawler/job/urljob.py
@@ -8,7 +8,6 @@ class UrlJob(Job):
 
     def __init__(self, statistics, bloom_filters_number: int, bloom_filters_capacity: int):
         super().__init__("urls", statistics)
-        self._hash = self._default_hash
         self._bloom = Bloom(n=bloom_filters_number,
                             capacity=bloom_filters_capacity)
 
@@ -16,17 +15,8 @@ class UrlJob(Job):
         super()._update_put_statistics(value)
         self._statistics.add(self._name, "Total %s" % self._name)
 
-    def _default_hash(self, value):
-        """Return the hash of given value. Default is self."""
-        return value
-
-    def set_hash(self, _hash):
-        """Set hash function."""
-        self._hash = _hash
-
     def put(self, value):
         """Add element to jobs using dictionary keys."""
-        key = self._hash(value)
-        if key not in self._bloom:
+        if value not in self._bloom:
             self._bloom.put(value)
             super().put(value)
