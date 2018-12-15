5c6b8fcde294e1232932e1a9d536b9ad633a19b3
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-12-15 08:02:54 +0100

    Added usage counter to Usable

--- tinycrawler/collections/usable.py
+++ tinycrawler/collections/usable.py
@@ -4,9 +4,13 @@ import abc
 class Usable(object):
     __metaclass__ = abc.ABCMeta
 
+    def __init__(self, **kwargs):
+        self._usages = 0
+
     @abc.abstractmethod
-    def used(self):
+    def used(self, **kwargs):
         """Define what to do after object has been used."""
+        self._usages += 1
 
     @abc.abstractmethod
     def use(self):
