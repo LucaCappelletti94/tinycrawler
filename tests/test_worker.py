5ea5a99ed474de1f90b7e6bc9ac16ad9d7c295a8
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-12-24 12:04:59 +0100

    Added worker class and its tests

---
+++ tests/test_worker.py
@@ -0,0 +1,42 @@
+from tinycrawler.processes.client.worker import Worker
+from tinycrawler.expirables import TasksQueue, DownloaderTask, Proxy, Url, ClientData
+from tinycrawler.utils import Logger, ProxyData
+from .utils import mock_ip_success
+from multiprocessing import Event
+from httmock import HTTMock
+import json
+
+
+def test_worker():
+    e = Event()
+    path = "logs/test_queue_process.log"
+    errors = Logger(path)
+
+    tasks = TasksQueue(DownloaderTask)
+    completed_tasks = TasksQueue(DownloaderTask)
+
+    with HTTMock(mock_ip_success):
+        client_data = ClientData(3)
+
+    with open("test_data/raw_proxy_data.json", "r") as f:
+        proxy_data = ProxyData(data=json.load(f))
+
+    url = Url("https://travis-ci.org/LucaCappelletti94/tinycrawler/builds/468601955")
+    proxy = Proxy(proxy_data, maximum_usages=1)
+
+    downloader_task = DownloaderTask(proxy, url, 0)
+
+    tasks.add(downloader_task)
+
+    worker = Worker(
+        client_data,
+        tasks,
+        completed_tasks,
+        stop=e,
+        logger=errors
+    )
+
+    worker._sink(downloader_task)
+
+    assert completed_tasks.pop(client_data.ip) == downloader_task
+    assert worker._source() == downloader_task
--- tinycrawler/processes/client/worker.py
+++ tinycrawler/processes/client/worker.py
@@ -1,15 +1,16 @@
 from ..queue_process import QueueProcess
-from ...expirables import TasksQueue, Task
+from ...expirables import TasksQueue, Task, ClientData
 
 
 class Worker(QueueProcess):
-    def __init__(self, tasks: TasksQueue, completed_tasks: TasksQueue, *args, **kwargs):
+    def __init__(self, client_data: ClientData, tasks: TasksQueue, completed_tasks: TasksQueue, *args, **kwargs):
         super(Worker, self).__init__(*args, **kwargs)
         self._tasks = tasks
         self._completed_tasks = completed_tasks
+        self._client_data = client_data
 
     def _sink(self, completed_task: Task):
         self._completed_tasks.add(completed_task)
 
     def _source(self):
-        return self._tasks.pop()
+        return self._tasks.pop(self._client_data.ip)
