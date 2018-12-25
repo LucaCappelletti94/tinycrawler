797ac526066ca23c4997203939cced8a64d5906a
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-12-25 09:50:55 +0100

    Extended clients test

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -1,28 +1,24 @@
 """Test to verify that class CLients has everything in order."""
 from tinycrawler.data import Clients
-from tinycrawler import IllegalArgumentError
 from tinycrawler.expirables import ClientData
-from .utils import mock_ip_success
+from .utils import mock_ip_success, mock_repr
 from httmock import HTTMock
 
 
-def test_clients():
+def setup():
     with HTTMock(mock_ip_success):
         clients = Clients()
-        client = ClientData(clients.get_new_client_id())
+        clients.register(ClientData(clients.get_new_client_id()))
+        return clients
+
+
+def test_clients():
+    clients = setup()
+    with HTTMock(mock_ip_success):
         client2 = ClientData(clients.get_new_client_id())
-        assert clients.is_new_ip(client.ip)
-        clients.register(client)
-        try:
-            clients.register(client)
-            assert False
-        except IllegalArgumentError:
-            pass
+    assert not clients.is_new_ip(client2.ip)
+    assert client2 not in clients
 
-        assert not clients.is_new_ip(client.ip)
 
-        assert client in clients
-        assert client2 not in clients
-
-        with open("test_data/expected_clients_representation.json", "r") as f:
-            assert str(clients) == f.read()
+def test_clients_repr():
+    mock_repr(setup())
