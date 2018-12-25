1ae09a1a27610a6bd370dac3aa8abcd6a777a1e5
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-12-25 09:19:43 +0100

    Updated client data representation

--- tests/test_client_data.py
+++ tests/test_client_data.py
@@ -1,12 +1,17 @@
 """Test to see if everything is working within ip."""
 from tinycrawler.expirables import ClientData
 from httmock import HTTMock
-from .utils import mock_ip_success
+from .utils import mock_ip_success, mock_repr
+
+
+def setup():
+    with HTTMock(mock_ip_success):
+        return ClientData(3)
 
 
 def test_client_data():
-    path = "test_data/expected_client_data_representation.json"
-    with HTTMock(mock_ip_success):
-        assert ClientData(3) == ClientData(3)
-        with open(path, "r") as f:
-            assert str(ClientData(0)) == f.read()
+    assert setup() == setup()
+
+
+def test_client_data_repr():
+    mock_repr(setup())
