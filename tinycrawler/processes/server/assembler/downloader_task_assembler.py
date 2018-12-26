fdd7d3bcae25027aaaed35155606722c95137573
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-12-26 19:07:08 +0100

    Extended docs

--- tinycrawler/processes/server/assembler/downloader_task_assembler.py
+++ tinycrawler/processes/server/assembler/downloader_task_assembler.py
@@ -1,3 +1,4 @@
+"""Define a process that creates downloader tasks."""
 from .task_assembler import TaskAssembler
 from ....data import Urls, Proxies
 from ....expirables import Url, Proxy, Domain, DownloaderTask
@@ -7,7 +8,10 @@ from typing import Tuple
 
 
 class DownloaderTaskAssembler(TaskAssembler):
+    """Define a process that creates downloader tasks."""
+
     def __init__(self, urls: Urls, proxies: Proxies, **kwargs):
+        """Define a process that creates downloader tasks."""
         super(DownloaderTaskAssembler, self).__init__(**kwargs)
         self._urls = urls
         self._proxies = proxies
