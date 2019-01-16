9dd56946fe772971fa1e9d2b33c01bee5b1c7df9
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-01-16 22:20:44 +0100

    Create test for parser task assembler manager

---
+++ tests/processes_managers/test_parser_task_assembler_manager.py
@@ -0,0 +1,29 @@
+from tinycrawler.process_managers import ParserTaskAssemblerManager
+from ..expirables.test_response import setup as response_setup
+from ..managers.test_client_crawler_manager import setup as client_crawler_manager_setup
+import time
+
+
+def setup():
+    ccm = client_crawler_manager_setup()
+    manager = ParserTaskAssemblerManager(
+        stop=ccm.end_event,
+        logger=ccm.logger,
+        max_waiting_timeout=10,
+        max_processes=8,
+        tasks=ccm.parser_tasks,
+        task_kwargs={},
+        responses=ccm.responses
+    )
+    return manager, ccm
+
+
+def test_parser_task_assembler_manager():
+    manager, ccm = setup()
+    response = response_setup()
+    ccm.responses.add(response)
+    manager.update()
+    time.sleep(2)
+    ccm.end_event.set()
+    manager.join()
+    assert ccm.parser_tasks.pop()
